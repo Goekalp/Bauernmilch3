442d660cf5bd38ea6533edf23bf5c0b130c5e0d4
Goekalp <Goekalpyildiz@outlook.de>
2021-05-21 14:31:11 +0200

    alle bugs gefixed

--- app/daos/ProduktDao.scala
+++ app/daos/ProduktDao.scala
@@ -2,12 +2,13 @@ package daos
 
 import model.Produkt
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
+import slick.jdbc.JdbcProfile
 
 import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
 
-class ProduktDao @Inject()(protected val dbConfigProvider: DataBaseConfigProvider)
+class ProduktDao @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)
                           (implicit executionContext: ExecutionContext) extends HasDatabaseConfigProvider[JdbcProfile] {
 
   import profile.api._
